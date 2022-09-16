d6bb9064a88841201df9d626dba69f9a2b686bda
Ezemonye <enochezemonye@gmail.com>
2022-09-16 15:07:15 -0700

    upper case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,11 +5,11 @@
  * @c: input
  *  Return: 1 if c is uppercase, 0 otherwise
  */
-
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	if (c > 64 && c < 91)
 		return (1);
+
 	else
 		return (0);
 }
