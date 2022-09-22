6fe6c6b2b07070a20ea838f18cb71cad84439d74
Ezemonye <enochezemonye@gmail.com>
2022-09-22 07:46:10 -0700

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,15 +9,12 @@ include "main.h"
  */
 void reverse_array(int *a, int n)
 {
-	int i = 0, t;
+	int tmp, index;
 
-	n = n - 1;
-	while (i < n)
+	for (index = n - 1; index >= n / 2; index--)
 	{
-		t = *(a + i);
-		*(a + i) = *(a + n);
-		*(a + n) = t;
-		i++;
-		n--;
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
 	}
 }
