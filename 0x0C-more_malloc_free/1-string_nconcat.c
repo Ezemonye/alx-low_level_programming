e3bb0de6c108e5014c883c30264fbe02892d2f96
Ezemonye <enochezemonye@gmail.com>
2022-10-06 12:42:24 -0700

    concatenate string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,39 +10,29 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *arr;
-	unsigned int i, j, co, co_2;
+	char *nstr, *empt;
+	unsigned int i, len, j;
+	unsigned int size;
 
+	len = 0;
+	empt = "";
 	if (s1 == NULL)
-		s1 = "";
+		s1 = empt;
 	if (s2 == NULL)
-		s2 = "";
-
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
-
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
-
-	if (n < j)
-		j = n;
-
-	j += i;
-	arr = malloc(sizeof(char *) * (j + 1));
-
-	if (arr == NULL)
+		s2 = empt;
+	while (s1[len] != '\0')
+		len++;
+	size = (len + n) * sizeof(*nstr);
+	nstr = malloc(size + 1);
+	if (nstr == NULL)
 		return (NULL);
-
-	for (co = 0; co < i; co++)
-		arr[co] = s1[co];
-	for (co_2 = 0; co < j; co_2++)
+	i = 0;
+	while (i < size && s1[i] != '\0')
 	{
-		arr[co] = s2[co_2];
-		co++;
+		nstr[i] = s1[i];
+		i++;
+		j++;
 	}
-	co++;
-	arr[co] = '\0';
-	return (arr);
+	nstr[i] = '\0';
+	return (nstr);
 }
