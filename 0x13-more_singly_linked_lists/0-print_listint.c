7ebe62e457b8cf8c56663056c6a59373e6669375
Ezemonye <enochezemonye@gmail.com>
2022-10-25 06:56:35 -0700

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,20 +2,19 @@
 
 /**
  * print_listint - function with one argument
- * @h: pointer to struct
- *
- * Description: prints all the elements of a list
- * Return: number of nodes
+ * Return: size_t
+ * @h: list
  */
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
-
-	while (h != NULL)
+	int nodes = 0;
+	
+	while (h)
 	{
-		printf("%d\n", h->n);
+		nodes++;
+		printf("%d", h->n);
 		h = h->next;
-		count++;
+		printf("\n");
 	}
-	return (count);
+	return (nodes);
 }
